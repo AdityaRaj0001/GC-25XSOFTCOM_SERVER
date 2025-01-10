d09cfc1f4c9fe28f8cf64b100326a5e9bc453277
Kartik Tiwari <mekartik27@gmail.com>
2025-01-11 02:48:07 +0530

    JWT token Swagger

--- src/main.ts
+++ src/main.ts
@@ -9,9 +9,25 @@ async function bootstrap() {
     .setDescription('This is the description of the API')
     .setVersion('1.0')
     .addTag('cats')
+    .addBearerAuth(
+      {
+        type: 'http',
+        scheme: 'bearer',
+        bearerFormat: 'JWT',
+        name: 'JWT',
+        description: 'Enter JWT token',
+        in: 'header',
+      },
+      'JWT-auth', // This name here is important for matching up with @ApiBearerAuth() in your controller!
+    )
     .build();
-  const documentFactory = () => SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, documentFactory);
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document, {
+    swaggerOptions: {
+      persistAuthorization: true, // this helps to retain the token even after refreshing the (swagger UI web page)
+      defaultModelsExpandDepth: 0,
+    },
+  });
   await app.listen(4000);
 }
 bootstrap();
