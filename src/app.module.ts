18db6264ef9ece5bbd3898fe65724e61e358a020
Arpit078 <verma.arpit078@gmail.com>
2025-01-11 03:07:36 +0530

    coordinator

--- src/app.module.ts
+++ src/app.module.ts
@@ -10,6 +10,7 @@ import { UsersModule } from './users/users.module';
 import { AdminService } from './admin/admin.service';
 import { AdminController } from './admin/admin.controller';
 import { AdminModule } from './admin/admin.module';
+import { CoordinatorModule } from './coordinator/coordinator.module';
 
 @Module({
   imports: [
@@ -17,7 +18,8 @@ import { AdminModule } from './admin/admin.module';
     ConfigModule.forRoot({ isGlobal: true }),
     AuthModule,
     UsersModule,
-    AdminModule
+    AdminModule,
+    CoordinatorModule
   ],
   controllers: [AppController, AdminController],
   providers: [AppService, AuthService, UsersService, AdminService],
